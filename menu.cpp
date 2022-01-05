898455e6186020c5397225432eccc2f0dda6a9e0
dandman <demoon.pps@gmail.com>
2022-01-05 16:41:21 +0100

    Writing menu still, button changes

--- menu.cpp
+++ menu.cpp
@@ -39,7 +39,7 @@ void menu::read_buttons()
 {
 	for (int i = 0; i < 4; i++)
 	{
-		if (digitalRead(button_[i]) == LOW)
+		if (digitalRead(menu_button_[i]) == LOW)
 			button_state_[i] = true;
 		else
 			button_state_[i] = false;
@@ -52,7 +52,7 @@ int menu::read_button()
 	while (true)
 	{
 		for (int i = 0; i < 4; i++)
-			if (digitalRead(button_[i]) == LOW)
+			if (digitalRead(menu_button_[i]) == LOW)
 				return i;
 		delay(15);
 	}
@@ -140,45 +140,163 @@ void menu::display_settings()
 	tft_->fillRect(0, 180, 240, 3, ILI9341_RED);
 }
 
+String menu::game_difficulty_to_string()
+{
+	if (game_difficulty_ == 0)
+		return "Easy";
+	if (game_difficulty_ == 1)
+		return "Normal";
+	return "Hard";
+}
+
 void menu::display_difficulty_settings()
 {
 	tft_->fillScreen(ILI9341_BLACK);
-	display_string(15, 35, 4, ILI9341_CYAN, "DIFFICULTY");
-	tft_->fillRect(0, 80, 240, 3, ILI9341_RED);
-	display_string(10, 120, 4, ILI9341_WHITE, String(game_difficulty_));
+	display_string(30, 45, 3, ILI9341_ORANGE, "DIFFICULTY");
+	tft_->fillRect(0, 95, 240, 3, ILI9341_RED);
+	display_string(60, 120, 4, ILI9341_CYAN, game_difficulty_to_string());
 	tft_->fillRect(0, 165, 240, 3, ILI9341_RED);
+}
+
+void menu::handle_difficulty_settings()
+{
+	display_difficulty_settings();
+	while (true)
+	{
+		switch (read_button())
+		{
+		case 0:
+			game_difficulty_ = (game_difficulty_ + 1) % 3;
+			tft_->fillRect(0, 120, 240, 40, ILI9341_BLACK);
+			display_string(60, 120, 4, ILI9341_CYAN, game_difficulty_to_string());
+			break;
+		case 2:
+			game_difficulty_ = (game_difficulty_ - 1) % 3;
+			tft_->fillRect(0, 120, 240, 40, ILI9341_BLACK);
+			display_string(60, 120, 4, ILI9341_CYAN, game_difficulty_to_string());
+			break;
+		default:
+			display_settings();
+			return;
+		}
+		delay(150);
+	}
+}
+
+String menu::button_to_string(const int button_number)
+{
+	switch (button_number)
+	{
+	case 7:
+		return "Upper button";
+	case 6:
+		return "Left button";
+	case 5:
+		return "Middle button";
+	case 4:
+		return "Right button";
+	default:
+		Serial.println("ERROR");
+		return "ERROR";
+	}
+}
+
+void menu::highlight_selected_element_button_settings()
+{
+	if (selected_menu_element_ == 0)
+	{
+		display_string(10, 95, 3, ILI9341_WHITE, "Move left:");
+		display_string(20, 130, 3, ILI9341_WHITE, button_to_string(game_button_[0]));
+		display_string(10, 160, 3, ILI9341_CYAN, "Move right:");
+		display_string(20, 195, 3, ILI9341_CYAN, button_to_string(game_button_[3]));
+		display_string(10, 225, 3, ILI9341_CYAN, "Shoot:");
+		display_string(20, 260, 3, ILI9341_CYAN, button_to_string(game_button_[1]));
+	}
+	else if (selected_menu_element_ == 1)
+	{
+		display_string(10, 95, 3, ILI9341_CYAN, "Move left:");
+		display_string(20, 130, 3, ILI9341_CYAN, button_to_string(game_button_[0]));
+		display_string(10, 160, 3, ILI9341_WHITE, "Move right:");
+		display_string(20, 195, 3, ILI9341_WHITE, button_to_string(game_button_[3]));
+		display_string(10, 225, 3, ILI9341_CYAN, "Shoot:");
+		display_string(20, 260, 3, ILI9341_CYAN, button_to_string(game_button_[1]));
+	}
+	else
+	{
+		display_string(10, 95, 3, ILI9341_CYAN, "Move left:");
+		display_string(20, 130, 3, ILI9341_CYAN, button_to_string(game_button_[0]));
+		display_string(10, 160, 3, ILI9341_CYAN, "Move right:");
+		display_string(20, 195, 3, ILI9341_CYAN, button_to_string(game_button_[3]));
+		display_string(10, 225, 3, ILI9341_WHITE, "Shoot:");
+		display_string(20, 260, 3, ILI9341_WHITE, button_to_string(game_button_[1]));
+	}
+}
+
+void menu::display_button_settings() //repair!!
+{
+	tft_->fillScreen(ILI9341_BLACK);
+	display_string(25, 35, 4, ILI9341_ORANGE, "BUTTONS");
+	tft_->fillRect(0, 80, 240, 3, ILI9341_RED);
+	display_string(10, 95, 3, ILI9341_WHITE, "Move left:");
+	display_string(20, 130, 3, ILI9341_WHITE, button_to_string(game_button_[0]));
+	tft_->fillRect(0, 145, 240, 3, ILI9341_RED);
+	display_string(10, 160, 3, ILI9341_CYAN, "Move right:");
+	display_string(20, 195, 3, ILI9341_CYAN, button_to_string(game_button_[3]));
+	tft_->fillRect(0, 210, 240, 3, ILI9341_RED);
+	display_string(10, 225, 3, ILI9341_CYAN, "Shoot:");
+	display_string(20, 260, 3, ILI9341_CYAN, button_to_string(game_button_[1]));
+	tft_->fillRect(0, 275, 240, 3, ILI9341_RED);
+}
+
+void menu::handle_button_change()
+{
+
+}
+
+void menu::handle_buttons_settings()
+{
+	selected_menu_element_ = 0;
+	display_button_settings();
+	while (true)
+	{
+		switch (read_button())
+		{
+		case 0:
+			selected_menu_element_ = (selected_menu_element_ - 1) % 3;
+			highlight_selected_element_button_settings();
+			break;
+		case 1:
+			handle_button_change();
+			break;
+		case 2:
+			selected_menu_element_ = (selected_menu_element_ + 1) % 3;
+			highlight_selected_element_button_settings();
+			break;
+		case 3:
+			selected_menu_element_ = 0;
+			display_settings();
+			return;
+		default:
+			Serial.println("ERROR");
+			return;
+		}
+	}
 }
 
 void menu::handle_settings_select()
 {
 	if (selected_menu_element_ == 0)
-	{
-		display_difficulty_settings();
-		while (true)
-		{
-			switch (read_button())
-			{
-			case 0:
-				break;
-			case 2:
-				break;
-			default:
-				return;
-			}
-		}
-	}
+		handle_difficulty_settings();
 	else
-	{
-
-	}
+		handle_buttons_settings();
 }
 
 void menu::handle_settings()
 {
 	selected_menu_element_ = 0;
+	display_settings();
 	while (true)
 	{
-		display_settings();
 		const int button = read_button();
 		if (button == 0)
 		{
--- menu.h
+++ menu.h
@@ -4,15 +4,15 @@
 class menu
 {
 	const Adafruit_ILI9341* tft_;
-	//in menu : 0-play, 1-settings, 2-authors| in setting : 0-difficulty, 1-buttons
+	//in menu : 0-play, 1-settings, 2-authors | in setting : 0-difficulty, 1-buttons | in buttons : 0-left, 1-right, 2-shoot
 	int selected_menu_element_ = 0;
 	//3 game difficulties - 0,1,2
 	int game_difficulty_ = 0;
-	//default for menu - 0 up, 1 enter, 2 down, 3 back
-	int button_[4] = { BUTTON_PIN1,BUTTON_PIN2,BUTTON_PIN3,BUTTON_PIN4 };
-	//default for game - 0 shoot, 1 left, 2 shoot, 3 right
+	//for menu - 0 up, 1 enter, 2 down, 3 back
+	int menu_button_[4] = { BUTTON_PIN1,BUTTON_PIN2,BUTTON_PIN3,BUTTON_PIN4 };
+	//default for game - 0 shoot, 1 left, 2 - , 3 right
 	int game_button_[4] = { BUTTON_PIN1,BUTTON_PIN2,BUTTON_PIN3,BUTTON_PIN4 };
-	bool button_state_[4] = { false,false,false,false }; //true-pressed
+	bool button_state_[4] = { false,false,false,false };
 public:
 	explicit menu(const Adafruit_ILI9341* tft);
 	void display_main_menu();
@@ -27,7 +27,14 @@ private:
 	void display_authors();
 	void highlight_selected_element_settings() const;
 	void display_settings();
-	static void display_difficulty_settings();
+	String game_difficulty_to_string();
+	void display_difficulty_settings();
+	void handle_difficulty_settings();
+	String button_to_string(int button_number);
+	void highlight_selected_element_button_settings();
+	void display_button_settings();
+	void handle_button_change();
+	void handle_buttons_settings();
 	void handle_settings_select();
 	void handle_settings();
 	void handle_menu_select();
